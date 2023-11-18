ebfbe2f615ababf502c9663e18facc4b20cc949e
Piotr Matyjasik <matyjasik.piotr@wp.pl>
2023-11-18 21:02:45 +0100

    Add experience section (#5)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import { AboutSection } from '@/sections/AboutSection';
+import { ContactSection } from '@/sections/ContactSection';
+import { ExperienceSection } from '@/sections/ExperienceSection';
 import { HeroSection } from '@/sections/HeroSection';
+import { ProjectsSection } from '@/sections/ProjectsSection';
 
 const Home = () => (
   <>
     <HeroSection />
     <AboutSection />
+    <ExperienceSection />
+    <ProjectsSection />
+    <ContactSection />
   </>
 );
 
