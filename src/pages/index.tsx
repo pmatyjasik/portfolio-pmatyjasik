4210f7603c56f9de422a534091c14a304c60dc01
Piotr Matyjasik <matyjasik.piotr@wp.pl>
2023-11-18 21:53:37 +0100

    Add og-image and description (#9)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,13 @@ import { ExperienceSection } from '@/sections/ExperienceSection';
 import { Footer } from '@/sections/Footer';
 import { HeroSection } from '@/sections/HeroSection';
 import { ProjectsSection } from '@/sections/ProjectsSection';
+import Head from 'next/head';
 
 const Home = () => (
   <>
+    <Head>
+      <title>Piotr Matyjasik</title>
+    </Head>
     <HeroSection />
     <AboutSection />
     <ExperienceSection />
