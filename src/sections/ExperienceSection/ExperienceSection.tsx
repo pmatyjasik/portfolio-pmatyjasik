ff67ed564c64840d1d6d2874504a11a9892e02b6
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2023-11-19 22:00:36 +0100

    Fix errors in descriptions

--- src/sections/ExperienceSection/ExperienceSection.tsx
+++ src/sections/ExperienceSection/ExperienceSection.tsx
@@ -22,27 +22,27 @@ export const ExperienceSection = () => {
           company='WIRTUALNA POLSKA'
           position='Front-end Develper'
           description='My role at the company involves developing and bug fixing. I actively take part in numerous projects, including creating user interfaces, implementing new functionalities, and resolving issues. I collaborate with the development team and system architects, participating in defining project architecture and establishing project requirements.'
-          technologies='React, Next.js, Javascript, Typescript, HTML, CSS, TailwindCSS, SCSS, Git, REST API, GraphQL, MobX, MobX-StateTree, Jotai, Jest, React Testing Library.'
+          technologies='React, Next.js, Javascript, Typescript, HTML, CSS, TailwindCSS, SCSS, Git, REST API, GraphQL, MobX, MobX-StateTree, Jotai, Jest, React Testing Library'
         />
         <TimelineItem
           date='September 2022 - May 2023'
           company='FUJITSU'
           position='Front-end Develper'
           description={`I actively participated in the development of an application for displaying and manipulating complex data, as well as resolving issues on both the frontend and backend. I also collaborated in defining the application's requirements.`}
-          technologies='Technologies: React, Javascript, Typescript, Git, SQL, REST API, CSS, HTML.'
+          technologies='Javascript, Typescript, React, Git, SQL, REST API, CSS, HTML'
         />
         <TimelineItem
-          date='June 2022 - September 2022'
+          date='June 2021 - September 2022'
           company='ETECHNOLOGIE'
           position='Front-end Develper'
           description='I took a central role in the creation and development of e-learning and e-commerce platforms, which contributed to their design, implementation, and continuous improvement. I also took part in defining project requirements.'
-          technologies='Javascript, Git, React, Wordpress, PHP, HTML, CSS, REST API.'
+          technologies='Javascript, React, Wordpress, PHP, HTML, CSS, REST API, Git'
         />
         <TimelineItem
-          date='October 2021 - June 2022'
+          date='October 2020 - June 2021'
           company='OIRP SZCZECIN'
           position='IT Support'
-          description='Technical assistance during online lessons, Technical assistance during stationary events, Student file management'
+          description='I provided technical assistance and troubleshooting during classroom and online activities. I was also responsible for managing student files.'
         />
       </motion.ol>
     </SectionLayout>
