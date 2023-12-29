8c0e88dacaf47d86d81026f56dc24e09f152776c
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2023-12-29 23:46:23 +0100

    Improve seo and accessibility

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta
           name='viewport'
-          content='width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0'
+          content='width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=3.0'
         />
       </Head>
       <Component {...pageProps} />
